9a70e9a4a0877dc002ddd88d4828696fcab07ca0
Max Jacobson <max@hardscrabble.net>
2017-12-02 11:32:54 -0500

    Add day-02-star-01 solution

--- day-02-star-01/src/main.rs
+++ day-02-star-01/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", checksum("puzzle_input.txt"))
+}
+
+fn checksum(filename: &str) -> u32 {
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect(
+        "something went wrong reading the file",
+    );
+
+    contents.lines().fold(0, |accumulator, line| {
+        let mut values: Vec<u32> = line.split_whitespace()
+            .map(|number_string| {
+                number_string.parse::<u32>().expect("could not parse")
+            })
+            .collect();
+        values.sort();
+        let largest_value = values.last().unwrap();
+        let smallest_value = values.first().unwrap();
+        let delta = largest_value - smallest_value;
+
+        accumulator + delta
+    })
+}
+
+#[test]
+fn example() {
+    assert_eq!(checksum("example.txt"), 18)
+}
+
+#[test]
+fn example_2() {
+    assert_eq!(checksum("example2.txt"), 19)
 }
